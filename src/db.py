fbdede919041c6fdd30590ba51b8d8ef96c62bfb
Johannes Schriewer <j.schriewer@anfe.ma>
2016-12-02 18:25:58 +0100

    Revert some errors from last commit

--- src/db.py
+++ src/db.py
@@ -1,13 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from .model import Model
-import sqlite3
 
 def create_engines(files):
 	engines = []
 	for file in files:
-		creator = lambda: sqlite3.connect('file:' + file + '?mode=ro', uri=True)
-		engine = create_engine('sqlite:///' , engine_kwargs={'creator': creator})
+		engine = create_engine('sqlite:///' + file)
 		# Model.metadata.create_all(engine)
 		s = sessionmaker(engine)
 		setattr(s, 'file', file)
